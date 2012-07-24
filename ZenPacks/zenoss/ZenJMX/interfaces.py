19d8ed98fda4de17cfd5f1faa45feeeb862db999
gstreiff <gstreiff@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-07-24 05:16:47 +0000

    [trunk/core] update source headers per legal for 4.2.x and beyond using Summer's script fu; reviewed by gstreiff jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.ZenJMX@61801 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/ZenJMX/interfaces.py
+++ ZenPacks/zenoss/ZenJMX/interfaces.py
@@ -1,15 +1,13 @@
-###########################################################################
-#
-# This program is part of Zenoss Core, an open source monitoring platform.
-# Copyright (C) 2010, Zenoss Inc.
-#
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 2 or (at your
-# option) any later version as published by the Free Software Foundation.
-#
-# For complete information please visit: http://www.zenoss.com/oss/
-#
-###########################################################################
+##############################################################################
+# 
+# Copyright (C) Zenoss, Inc. 2010, all rights reserved.
+# 
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+# 
+##############################################################################
+
+
 from Products.Zuul.interfaces import IRRDDataSourceInfo
 from Products.Zuul.form import schema
 from Products.Zuul.utils import ZuulMessageFactory as _t
@@ -35,6 +33,3 @@ class IJMXDataSourceInfo(IRRDDataSourceInfo):
     operationParamValues = schema.TextLine(title=_t(u'Parameter Values'), group=_t(u'JMX Attribute and Operation Configuration'))
     operationName = schema.TextLine(title=_t(u'Operation Name'), group=_t(u'JMX Attribute and Operation Configuration'))
     operationParamTypes = schema.TextLine(title=_t(u'Parameter Types'), group=_t(u'JMX Attribute and Operation Configuration'))
-    
-    
-
--- ZenPacks/zenoss/ZenJMX/src/main/java/com/zenoss/zenpacks/zenjmx/ZenJmxMain.java
+++ ZenPacks/zenoss/ZenJMX/src/main/java/com/zenoss/zenpacks/zenjmx/ZenJmxMain.java
@@ -1,17 +1,13 @@
-///////////////////////////////////////////////////////////////////////////
-//
-//Copyright 2008 Zenoss Inc 
-//Licensed under the Apache License, Version 2.0 (the "License"); 
-//you may not use this file except in compliance with the License. 
-//You may obtain a copy of the License at 
-//    http://www.apache.org/licenses/LICENSE-2.0 
-//Unless required by applicable law or agreed to in writing, software 
-//distributed under the License is distributed on an "AS IS" BASIS, 
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-//See the License for the specific language governing permissions and 
-//limitations under the License.
-//
-///////////////////////////////////////////////////////////////////////////
+/*****************************************************************************
+ * 
+ * Copyright (C) Zenoss, Inc. 2008, all rights reserved.
+ * 
+ * This content is made available according to terms specified in
+ * License.zenoss under the directory where your Zenoss product is installed.
+ * 
+ ****************************************************************************/
+
+
 package com.zenoss.zenpacks.zenjmx;
 
 import org.apache.commons.cli.CommandLine;
